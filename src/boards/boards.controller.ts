39c1dc803375868320badc914065389b85a1f516
hminn <hmkim0008@gmail.com>
2021-12-28 10:13:04 +0900

    Create 기능 구현
    - uuid 패키지 설치
    - Create 기능 구현 (service, controller 로직)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoards() {
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(@Body('title') title, @Body('description') description): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
