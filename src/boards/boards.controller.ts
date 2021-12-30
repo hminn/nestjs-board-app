e51a68353349fd1deb1ab82e667d62548658a45a
hminn <hmkim0008@gmail.com>
2021-12-30 18:01:01 +0900

    CreateBoard 작업에 대한 Dto 정의 및 코드 수정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,7 +13,7 @@ export class BoardsController {
   }
 
   @Post('/')
-  createBoard(@Body('title') title, @Body('description') description): Board {
-    return this.boardsService.createBoard(title, description);
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
   }
 }
