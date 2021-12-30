1755c78958b55a5d847c2ca03983ac491a2b206c
hminn <hmkim0008@gmail.com>
2021-12-30 19:58:11 +0900

    게시물 조회 및 삭제에 대한 예외 처리 작업 추가
    - 조회하고자 하는 id에 해당하는 게시물이 없는 경우에 대한 예외 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -12,7 +12,11 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const foundBoard = this.boards.find((board) => board.id === id);
+    if (!foundBoard) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return foundBoard;
   }
 
   createBoard(createBoardDto: CreateBoardDto) {
@@ -28,6 +32,7 @@ export class BoardsService {
   }
 
   deleteBoard(id: string): void {
+    this.getBoardById(id);
     this.boards = this.boards.filter((board) => board.id !== id);
   }
 
